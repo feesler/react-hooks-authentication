59dd2fab463ca1c37bf6e6ad3d622c871e5516bc
Ilya Makarov <feesler@gmail.com>
2021-03-10 18:36:07 +0300

    * Use context consumer for practice

--- frontend/src/components/Content/Content.jsx
+++ frontend/src/components/Content/Content.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import useAuth from '../../hooks/useAuth.js';
+import AuthContext from '../../contexts/AuthContext.js';
 import Banner from '../Banner/Banner.jsx';
 import NewsList from '../NewsList/NewsList.jsx';
 
 function Content(props) {
-  const { profile } = useAuth();
-
-console.log('[Content] profile: ', profile);
-
-  const content = (profile)
-    ? <NewsList />
-    : <Banner />;
-
   return (
     <div className="content">
-      {content}
+      <AuthContext.Consumer>
+        {value => (value.profile) ? <NewsList /> : <Banner />}
+      </AuthContext.Consumer>
     </div>
   )
 }
