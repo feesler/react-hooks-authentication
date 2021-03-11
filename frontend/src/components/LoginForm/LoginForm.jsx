59b1a4a556077a24d0e94f54178eac891a861d2a
Ilya Makarov <feesler@gmail.com>
2021-03-11 04:38:22 +0300

    + Add validation of login form

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames'
 import useAuth from '../../hooks/useAuth.js';
 
 const initialState = {
@@ -7,11 +8,18 @@ const initialState = {
   password: '',
 };
 
+const initialValidation = {
+  login: true,
+  password: true,
+};
+
 function LoginForm(props) {
-  const [state, setState] = useState(initialState)
+  const [state, setState] = useState(initialState);
+  const [validation, setValidation] = useState(initialValidation);
   const { profile, logIn } = useAuth();
 
   const handleChange = (e) => {
+    setValidation(initialValidation);
     setState((prev) => ({
       ...prev,
       [e.target.name]: e.target.value,
@@ -20,6 +28,15 @@ function LoginForm(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!state.login.length) {
+      setValidation((prev) => ({ ...prev, login: false }));
+      return;
+    }
+    if (!state.password.length) {
+      setValidation((prev) => ({ ...prev, password: false }));
+      return;
+    }
 
     logIn(state.login, state.password);
   }
@@ -30,22 +47,28 @@ function LoginForm(props) {
 
   return (
     <form className="login-form" onSubmit={handleSubmit}>
-      <input
-        className="form-control me-2"
-        type="text"
-        name="login"
-        onChange={handleChange}
-        value={state.login}
-        placeholder="Username"
-      />
-      <input
-        className="form-control me-2"
-        type="password"
-        name="password"
-        onChange={handleChange}
-        value={state.password}
-        placeholder="Password"
-      />
+      <div className="col me-2">
+        <input
+          className={classNames('form-control', { 'is-invalid': !validation.login })}
+          type="text"
+          name="login"
+          onChange={handleChange}
+          value={state.login}
+          placeholder="Username"
+        />
+        <div className="invalid-feedback">Input login</div>
+      </div>
+      <div className="col me-2">
+        <input
+          className={classNames('form-control', { 'is-invalid': !validation.password })}
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={state.password}
+          placeholder="Password"
+        />
+        <div className="invalid-feedback">Input password</div>
+      </div>
       <button className="btn btn-outline-success" type="submit">Login</button>
     </form>
   )
